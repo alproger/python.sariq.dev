5d9c1a498298961b26965ce61d9f82cacfd3b574
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-16 11:40:50 +0500

    Update amaliyot6_1.py

--- #6_SONLAR/amaliyot6_1.py
+++ #6_SONLAR/amaliyot6_1.py
@@ -1,7 +1,18 @@
 son  = int(input('son kiriting : '))
 
-#sonning kvadrati
+#.................1-usul.......................
+
+#sonning kvadrati 
 print('sonning kvadrati : {}'.format(son ** 2))
 
 #sonning kubi
 print('sonning kubi : {}'.format(son ** 3))
+
+
+#................2-usul.........................
+
+#sonning kvadrati va kubi 
+#print(f'sonning kvadrati : {son ** 2} \
+#  \n sonning kubi : {son ** 3}')
+
+...............................................
