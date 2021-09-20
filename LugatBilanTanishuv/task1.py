08deacd8e46ed397aaa3548c9179121b43f9c682
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-20 18:11:30 +0500

    dicts

--- LugatBilanTanishuv/task1.py
+++ LugatBilanTanishuv/task1.py
@@ -1,34 +1,46 @@
 friend = {
-    name : 'Akmal Xoshimov',
-    born : 1998,
-    years : 23,
-    city : 'Quva',
-    job : 'sotuvchi'
+    'name' : 'Akmal Xoshimov',
+    'born' : 1998,
+    'years' : 23,
+    'city' : 'Quva',
+    'job' : 'sotuvchi'
             }
 
 brother = {
-    name : 'Abbosxon Qodirov',
-    born : 2001,
-    years : 20,
-    city : 'Quva',
-    job : 'Sportchi'
+    'name' : 'Abbosxon Qodirov',
+    'born' : 2001,
+    'years' : 20,
+    'city' : 'Quva',
+    'job' : 'Sportchi'
             }
 
 i_am = {
-    name : 'Yaxyoxon Qodirov',
-    born : 1998,
-    years : 23,
-    city : 'Quva',
-    job : 'Talaba'
+    'name' : 'Yaxyoxon Qodirov',
+    'born' : 1998,
+    'years' : 23,
+    'city' : 'Quva',
+    'job' : 'Talaba'
         }
 
 teacher = {
-    name : 'Jahongir',
-    born : 1990,
-    years : 31,
-    city : 'Toshkent',
-    job : 'Dastur injineri'
+    'name' : 'Jahongir',
+    'born' : 1990,
+    'years' : 31,
+    'city' : 'Toshkent',
+    'job' : 'Dastur injineri'
 }
 
 print(f'Do\'stimning ismi {friend["name"]} {friend["born"]}-yilda {friend["city"]}da tug\'ulgan \
-    \{friend["year"]} yoshda kasbi {friend["job"]} ')
+{friend["years"]} yoshda kasbi {friend["job"]} ')
+
+
+print(f'Ukamning ismi {brother["name"]} {brother["born"]}-yilda {brother["city"]}da tug\'ulgan \
+{brother["years"]} yoshda kasbi {brother["job"]} ')
+
+
+print(f'O\'zimning ismim {i_am["name"]} {i_am["born"]}-yilda {i_am["city"]}da tug\'ulganman \
+{i_am["years"]} yoshda kasbim {i_am["job"]} ')
+
+
+print(f'Ustozimning ismi {teacher["name"]} {teacher["born"]}-yilda {teacher["city"]}da tug\'ulgan \
+{teacher["years"]} yoshda kasbi {teacher["job"]} ')
