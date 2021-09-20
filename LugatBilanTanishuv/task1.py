2cda26cf4f06f287210e13b427cbb80bd23e2835
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-20 18:01:35 +0500

    dicts

---
+++ LugatBilanTanishuv/task1.py
@@ -0,0 +1,34 @@
+friend = {
+    name : 'Akmal Xoshimov',
+    born : 1998,
+    years : 23,
+    city : 'Quva',
+    job : 'sotuvchi'
+            }
+
+brother = {
+    name : 'Abbosxon Qodirov',
+    born : 2001,
+    years : 20,
+    city : 'Quva',
+    job : 'Sportchi'
+            }
+
+i_am = {
+    name : 'Yaxyoxon Qodirov',
+    born : 1998,
+    years : 23,
+    city : 'Quva',
+    job : 'Talaba'
+        }
+
+teacher = {
+    name : 'Jahongir',
+    born : 1990,
+    years : 31,
+    city : 'Toshkent',
+    job : 'Dastur injineri'
+}
+
+print(f'Do\'stimning ismi {friend["name"]} {friend["born"]}-yilda {friend["city"]}da tug\'ulgan \
+    \{friend["year"]} yoshda kasbi {friend["job"]} ')
