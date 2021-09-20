fd2c9a9869698783b5cd3fed97825431ef3eb44c
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-20 11:06:48 +0500

    error tasks

---
+++ #12_XATOLAR_BILAN_ISHLASH/task7.py
@@ -0,0 +1,9 @@
+
+users = ['alisher1983','aziza','yasina', 'umar']  #, qolib ketgan 'yasina',
+
+login = input("Yangi login tanlang:" ) # " bo'lishi kerak edi
+
+if login in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
