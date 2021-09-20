a5fc17f99fee5ce547ac7dc2b3b6db4fc75ed727
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-20 18:56:58 +0500

    dict tasks

---
+++ LugatBilanTanishuv/task2.py
@@ -0,0 +1,10 @@
+taomlar = {
+    'Akmal'    : 'Osh',
+    'Yaxyoxon' : 'Qovoq manti',
+    'Abbos'    : 'Kabob',
+    'Sarvar'   : 'Rishton somsa',
+    'Murodil'  : 'Norin'
+}
+
+for ism,taom in taomlar.items():
+    print(f"{ism}ning sevimli taomi {taom}")
