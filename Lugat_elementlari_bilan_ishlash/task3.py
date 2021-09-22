3b5d4fb9071a6e63140755d00b4d17c50ce7ef81
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-22 09:34:32 +0500

    dict tasks

--- Lugat_elementlari_bilan_ishlash/task3.py
+++ Lugat_elementlari_bilan_ishlash/task3.py
@@ -12,7 +12,11 @@ counties = {
     'Palastina'  : 'Quddus'
 
 }
-print('World countries  |   Capitals   ')
-for key, value in  zip(sorted(counties.keys()), sorted(counties.values())) :
-    print(f'{key}            {value}')
-    
+
+state = input('Davlat nomini kiriting : ')
+
+if state.title() in counties.keys() :
+    print(f'{state.title()}ning poytaxti : {counties[state]}') 
+
+else:
+    print('Bizda bunday ma\'lumot yo\'q !')
