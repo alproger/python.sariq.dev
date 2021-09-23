a235507d7f834fa88bcf65fe166f06f2d156ab23
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-23 23:07:36 +0500

    while loop tasks

---
+++ WHILE_ROYXATLAR_VA_LUGATLAR/task2.py
@@ -0,0 +1,20 @@
+
+ebozor = {}
+print('e bozor uchun mahsulotlar va ularning narxini kiritamiz')
+
+while True:
+    
+    mahsulot = input('mahsulot nomini kiriting : ')
+    narx = input(f'{mahsulot}ning narxini kiriting : ')
+
+    ebozor[mahsulot] = int(narx)
+
+    savol = input('yana mahsulot qo\'shasizmi ? (ha/yo\'q) : ')
+
+    if savol == 'yo\'q' :
+        break
+
+for mahsulot, narx in ebozor.items():
+    info = f'{mahsulot}ning narxi : {narx}'
+    print(info)
+    
