1da34500c3b10efc4cc13a541428d93bb90df3e4
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-24 10:07:13 +0500

    Create task1.py

---
+++ FUNKSIYA/task1.py
@@ -0,0 +1,13 @@
+def tyil_hisobla(ism, yil):
+    '''Foydalanuvchi ismi va yoshini kiritsa tug'ulgan 
+       yilini hisoblab beruvchi funksiya '''
+    
+    tyil = 2021 - int(yil)
+    result = f"{ism} {tyil}-yilda tug'ulgansiz "
+
+    print(result)
+
+ism = input('ismingizni kiriting : ')
+yosh = input(f'{ism} yoshingizni kiriting : ')
+
+tyil_hisobla(ism, yosh)
