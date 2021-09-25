8f0ed7765edead48eba279d9e22509804ee953a9
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-25 06:07:56 +0500

    function tasks

---
+++ FUNKSIYA/task5.py
@@ -0,0 +1,12 @@
+def daraja_oshir( x, y):
+    '''Foydalanuvchidan x va y sonlarini olib, x^y ni 
+       konsolga chiqaruvchi funksiya  masalan 
+       x = 2, y = 3 natija 2^3 = 8'''
+
+    print(x ** y)
+
+print('x,y ni kiriting : ')
+x = int(input("x : "))
+y = int(input("y : "))
+
+daraja_oshir(x, y)
