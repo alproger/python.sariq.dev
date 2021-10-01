3919b3afdbdbfb9f6d721122dbd34fe76f6e4987
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-10-01 22:21:00 +0500

    class

---
+++ 28-KLASSLAR/task1.py
@@ -0,0 +1,12 @@
+
+#class User for registration to website new users 
+
+class User :
+    
+    def __init__(self, name, surname, username, mail, password):
+    
+        self.name = name
+        self.surname = surname
+        self.username = username
+        self.mail = mail
+        self.password = password
