c3d89e49f122e8d2d489019a3555e90f6ebeb272
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-10-04 11:05:16 +0500

    classes

---
+++ VORISLIK_VA_POLIMORFIZM/task1.py
@@ -0,0 +1,38 @@
+class Shaxs:
+    '''Shaxs klasi shaxsga oid malumotlar bilan ishlash uchun'''
+    
+    def __init__(self, ism, familya, passport, t_yil):
+        '''shaxs klasining hususiyatlari'''
+        
+        self.ism = ism
+        self.familya = familya
+        self.passport = passport
+        self.t_yil = t_yil
+        self.yosh = 2021 - int(t_yil)
+
+    def get_info(self):
+        '''Shaxsga tegishli hususiyatlarni qaytaradi'''
+        info = f'Shaxs {self.ism} {self.familya} passport : {self.passport} {self.t_yil}-yilda tug\'ulgan. {self.yosh} yoshda.'
+        return info
+    
+    def get_fullname(self):
+        '''shaxsga tegishli ism va familiyani qaytaradi'''
+        return ''.join(self.ism, self.familya)
+
+    def get_passport(self):
+        '''shaxsga tegishli passport malumotini qaytaradi'''
+        return self.passport
+    
+
+
+class Talaba(Shaxs):
+    '''shaxs klasidan nasl olgan talaba klasi. Bu klass talabalarning malumotlari bilan ishlash uchun'''
+
+    def __init__(self, ism, familya, passport, t_yil, universitet, talabaId):
+        '''Talaba klasining hususiyatlarini saqlovchi funksiya'''
+        super().__init__(ism, familya, passport, t_yil)
+        self.unversitet = universitet
+        self.talabaId = talabaId
+        self.fanlar = []
+
+        
